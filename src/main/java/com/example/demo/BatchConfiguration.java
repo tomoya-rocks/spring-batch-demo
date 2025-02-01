70b24dc0a5b64572b5a85bbf760aa4272a48abb7
Tomoya Sasaki <tomoya.let.it.bleed@gmail.com>
2025-02-01 22:13:33 +0900

    Use JDBC Writer instead of Console Writer

--- src/main/java/com/example/demo/BatchConfiguration.java
+++ src/main/java/com/example/demo/BatchConfiguration.java
@@ -1,7 +1,10 @@
 package com.example.demo;
 
+import javax.sql.DataSource;
+
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
+import org.springframework.batch.core.StepExecutionListener;
 import org.springframework.batch.core.job.builder.JobBuilder;
 import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.batch.core.step.builder.StepBuilder;
@@ -30,26 +33,26 @@ public class BatchConfiguration {
 	}
 
 	@Bean
-	public ItemWriter<FullNameMember> itemWriter() {
-		return chunk -> {
-			for (FullNameMember member : chunk) {
-				System.out.println(
-						member.id() + ":" + member.firstName() + ":" + member.lastName() + ":" + member.fullName());
-			}
-		};
+	public ItemWriter<FullNameMember> itemWriter(DataSource dataSource) {
+		return new MemberItemWriter(dataSource);
 	}
 
 	@Bean
-	public Job importMemberJob(JobRepository jobRepository, Step step1) {
-		return new JobBuilder("importMemberJob" + System.currentTimeMillis(), jobRepository).start(step1).build();
+	public StepExecutionListener stepExecutionListener(DataSource dataSource) {
+		return new MemberStepListener(dataSource);
 	}
 
 	@Bean
-	public Step step1(JobRepository jobRepository, DataSourceTransactionManager transactionManager,
+	public Job importMemberJob(JobRepository jobRepository, Step memberStep) {
+		return new JobBuilder("importMemberJob" + System.currentTimeMillis(), jobRepository).start(memberStep).build();
+	}
+
+	@Bean
+	public Step memberStep(JobRepository jobRepository, DataSourceTransactionManager transactionManager,
 			ItemReader<Member> reader, ItemProcessor<Member, FullNameMember> processor,
-			ItemWriter<FullNameMember> writer) {
+			ItemWriter<FullNameMember> writer, StepExecutionListener listener) {
 		return new StepBuilder("step1", jobRepository).<Member, FullNameMember>chunk(3, transactionManager)
-				.reader(reader).processor(processor).writer(writer).build();
+				.reader(reader).processor(processor).writer(writer).listener(listener).build();
 	}
 
 }
---
+++ src/main/java/com/example/demo/MemberItemWriter.java
@@ -0,0 +1,28 @@
+package com.example.demo;
+
+import javax.sql.DataSource;
+
+import org.springframework.batch.item.Chunk;
+import org.springframework.batch.item.ItemWriter;
+import org.springframework.jdbc.core.JdbcOperations;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+public class MemberItemWriter implements ItemWriter<FullNameMember> {
+
+	private final JdbcOperations jdbcTemplate;
+
+	public MemberItemWriter(DataSource dataSource) {
+		this.jdbcTemplate = new JdbcTemplate(dataSource);
+	}
+
+	@Override
+	public void write(Chunk<? extends FullNameMember> chunk) throws Exception {
+		for (FullNameMember fullNameMember : chunk) {
+			String sql = "insert into member values (?, ?, ?, ?)";
+
+			this.jdbcTemplate.update(sql, fullNameMember.id(), fullNameMember.firstName(), fullNameMember.lastName(),
+					fullNameMember.fullName());
+		}
+	}
+
+}
