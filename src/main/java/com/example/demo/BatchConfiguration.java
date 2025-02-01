d94d0976a907468c7b9a6b3d845dab3760f2012d
tomoya <tomoya@tomoyanoMacBook-Pro.local>
2025-02-01 21:27:00 +0900

    first commit

---
+++ src/main/java/com/example/demo/BatchConfiguration.java
@@ -0,0 +1,47 @@
+package com.example.demo;
+
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.job.builder.JobBuilder;
+import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.batch.core.step.builder.StepBuilder;
+import org.springframework.batch.item.ItemReader;
+import org.springframework.batch.item.ItemWriter;
+import org.springframework.batch.item.file.builder.FlatFileItemReaderBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+
+@Configuration
+public class BatchConfiguration {
+
+	@Bean
+	public ItemReader<Member> itemReader() {
+		return new FlatFileItemReaderBuilder<Member>().name("memberItemReader")
+				.resource(new ClassPathResource("member.csv")).delimited().names("id", "firstName", "lastName")
+				.linesToSkip(1).targetType(Member.class).build();
+	}
+
+	@Bean
+	public ItemWriter<Member> itemWriter() {
+		return chunk -> {
+			for (Member member : chunk) {
+				System.out.println(member.id() + ":" + member.firstName() + ":" + member.lastName());
+			}
+		};
+	}
+
+	@Bean
+	public Job importMemberJob(JobRepository jobRepository, Step step1) {
+		return new JobBuilder("importMemberJob" + System.currentTimeMillis(), jobRepository).start(step1).build();
+	}
+
+	@Bean
+	public Step step1(JobRepository jobRepository, DataSourceTransactionManager transactionManager,
+			ItemReader<Member> reader, ItemWriter<Member> writer) {
+		return new StepBuilder("step1", jobRepository).<Member, Member>chunk(3, transactionManager).reader(reader)
+				.writer(writer).build();
+	}
+
+}
---
+++ src/main/java/com/example/demo/Member.java
@@ -0,0 +1,4 @@
+package com.example.demo;
+
+public record Member(String id, String firstName, String lastName) {
+}
