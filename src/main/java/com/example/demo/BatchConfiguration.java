f4fd104dcc92faaaf55c238d9fdc8bfb912dbcd0
Tomoya Sasaki <t.sasaki@future-shop.co.jp>
2025-03-17 16:08:45 +0900

    Update

--- src/main/java/com/example/demo/BatchConfiguration.java
+++ src/main/java/com/example/demo/BatchConfiguration.java
@@ -1,12 +1,13 @@
 package com.example.demo;
 
+import javax.sql.DataSource;
+
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.StepScope;
 import org.springframework.batch.core.job.builder.JobBuilder;
 import org.springframework.batch.core.partition.support.Partitioner;
 import org.springframework.batch.core.repository.JobRepository;
-import org.springframework.batch.core.repository.support.ResourcelessJobRepository;
 import org.springframework.batch.core.step.builder.StepBuilder;
 import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.item.ExecutionContext;
@@ -14,38 +15,39 @@ import org.springframework.batch.item.ItemProcessor;
 import org.springframework.batch.item.ItemWriter;
 import org.springframework.batch.item.file.FlatFileItemReader;
 import org.springframework.batch.item.file.builder.FlatFileItemReaderBuilder;
-import org.springframework.batch.support.transaction.ResourcelessTransactionManager;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.orm.jpa.EntityManagerFactoryBuilder;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.task.SimpleAsyncTaskExecutor;
 import org.springframework.core.task.TaskExecutor;
-import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.transaction.PlatformTransactionManager;
 
 @Configuration
 public class BatchConfiguration {
 
 	@Bean
-	public ResourcelessTransactionManager transactionManager() {
-		return new ResourcelessTransactionManager();
-	}
-
-	@Bean(name = "jobRepository")
 	@Primary
-	public JobRepository jobRepository() {
-		return new ResourcelessJobRepository();
+	@ConfigurationProperties(prefix = "spring.datasource")
+	public DataSource dataSource() {
+		return org.springframework.boot.jdbc.DataSourceBuilder.create().build();
+	}
+	
+	@Bean
+	@Primary
+	public DataSourceTransactionManager transactionManager() {
+		return new DataSourceTransactionManager(dataSource());
 	}
 
 	@Bean
-	public Job importMemberJob(JobRepository jobRepository, PlatformTransactionManager platformTransactionManager,
-			EntityManagerFactoryBuilder.Builder emfBuilder) {
+	public Job importMemberJob(JobRepository jobRepository, PlatformTransactionManager transactionManager) {
 		return new JobBuilder("importMemberJob" + System.currentTimeMillis(), jobRepository)
-				.start(memberCsvTaskletStep(jobRepository, platformTransactionManager)).next(masterStep(jobRepository, emfBuilder))
-				.build();
+				.start(memberCsvTaskletStep(jobRepository, transactionManager))
+				.next(masterStep(jobRepository, transactionManager)).build();
 	}
 
 	@Bean
@@ -55,15 +57,15 @@ public class BatchConfiguration {
 	}
 
 	@Bean
-	public Step masterStep(JobRepository jobRepository, EntityManagerFactoryBuilder.Builder emfBuilder) {
+	public Step masterStep(JobRepository jobRepository, PlatformTransactionManager transactionManager) {
 		return new StepBuilder("master", jobRepository).partitioner("slaveStep", memberCsvPartitioner()).gridSize(10)
-				.step(slaveStep(jobRepository, emfBuilder)).taskExecutor(taskExecutor()).build();
+				.step(slaveStep(jobRepository, transactionManager)).taskExecutor(taskExecutor()).build();
 	}
 
 	@Bean
-	public Step slaveStep(JobRepository jobRepository, EntityManagerFactoryBuilder.Builder emfBuilder) {
-		return new StepBuilder("slave", jobRepository).<Member, FullNameMember>chunk(5000, transactionManager())
-				.reader(itemReader(null)).processor(itemProcessor()).writer(itemWriter(emfBuilder, null)).build();
+	public Step slaveStep(JobRepository jobRepository, PlatformTransactionManager dynamicTransactionManager) {
+		return new StepBuilder("slave", jobRepository).<Member, FullNameMember>chunk(5000, dynamicTransactionManager)
+				.reader(itemReader(null)).processor(itemProcessor()).writer(itemWriter(null)).build();
 	}
 
 	@Bean
@@ -81,16 +83,25 @@ public class BatchConfiguration {
 
 	@Bean
 	@StepScope
-	public ItemWriter<FullNameMember> itemWriter(EntityManagerFactoryBuilder.Builder emfBuilder,
+	public ItemWriter<FullNameMember> itemWriter(
 			@Value("#{stepExecutionContext['databaseConfig']}") DatabaseConfig databaseConfig) {
-		String url = String.format("jdbc:postgresql://%s/%s", databaseConfig.host(), databaseConfig.dbName());
-		DriverManagerDataSource dataSource = new DriverManagerDataSource();
-		dataSource.setUrl(url);
-		dataSource.setUsername(databaseConfig.username());
-		dataSource.setPassword(databaseConfig.password());
-		
+		return new MemberItemWriter(dynamicDataSource(databaseConfig));
+	}
 
-		return new MemberItemWriter(dataSource);
+	@Bean
+	@StepScope
+	public DataSource dynamicDataSource(
+			@Value("#{stepExecutionContext['databaseConfig']}") DatabaseConfig databaseConfig) {
+		return DataSourceBuilder.create()
+				.url(String.format("jdbc:postgresql://%s/%s", databaseConfig.host(), databaseConfig.dbName()))
+				.username(databaseConfig.username()).password(databaseConfig.password())
+				.driverClassName("org.postgresql.Driver").build();
+	}
+
+	@Bean
+	@StepScope
+	public DataSourceTransactionManager dynamicTransactionManager(DataSource dynamicDataSource) {
+		return new DataSourceTransactionManager(dynamicDataSource);
 	}
 
 	@Bean
